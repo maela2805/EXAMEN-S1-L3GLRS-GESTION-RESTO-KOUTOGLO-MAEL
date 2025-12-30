3d00998e43b04e14f067f03020491893f0779def
maelmaela <mael-maela.koutoglo@ism.edu.sn>
2025-12-30 13:07:47 +0000

    Index update

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 
+// use App\Kernel;
+
+// require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+// return function (array $context) {
+//     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+// }; 
+// 🔑 Forcer l'environnement PROD si non défini (Render)
+$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] ?? 'prod';
+$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] ?? '0';
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
