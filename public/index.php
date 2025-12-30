48040b6f10aecbf8f51b5e02ea8714767262cf5d
maelmaela <mael-maela.koutoglo@ism.edu.sn>
2025-12-30 13:49:15 +0000

    chagement de index

--- public/index.php
+++ public/index.php
@@ -8,13 +8,16 @@
 //     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 // }; 
 // 🔑 Forcer l'environnement PROD si non défini (Render)
+use App\Kernel;
+
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] ?? 'prod';
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] ?? '0';
-
-use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel(
+        $context['APP_ENV'],
+        (bool) $context['APP_DEBUG']
+    );
 };
